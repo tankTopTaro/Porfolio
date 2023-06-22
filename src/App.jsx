3c952c543b15409db34f95701712d7768b4f6541
Kevin Balmores <balmoresk3v1n@gmail.com>
2023-06-23 06:47:40 +0800

    changed the navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import HeroSection from "./components/HeroSection"
 import MyNavbar from "./components/Navbar"
 
 function App() {
@@ -5,6 +6,7 @@ function App() {
   return (
     <>
     <MyNavbar />
+    <HeroSection />
     </>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,43 +1,91 @@
-import { DarkThemeToggle, Navbar, Avatar } from 'flowbite-react';
-import Logo from './Logo';
+import { Fragment } from 'react'
+import { Disclosure, Menu, Transition } from '@headlessui/react'
+import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
+import { DarkThemeToggle } from 'flowbite-react'
+
+const navigation = [
+  { name: 'Home', href: '#', current: true },
+  { name: 'Projects', href: '#', current: false },
+  { name: 'Contact Me', href: '#', current: false },
+]
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ')
+}
 
 export default function MyNavbar() {
   return (
-    <Navbar
-      fluid
-      rounded
-    >
-      <Navbar.Brand href="#">
-        <Logo className="mr-3 h-6 sm:h-9" />
-        <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
-          TanktopTaro
-        </span>
-      </Navbar.Brand>
-      <div className="flex md:order-2 gap-2">
-        <DarkThemeToggle />
-      <Avatar alt="User settings" img="https://res.cloudinary.com/delgya5vj/image/upload/c_fill,g_face,w_300,h_300,r_max/v1687393571/doggos/My_project_2_zhawj7.png" rounded/>
-      </div>
-      <Navbar.Collapse>
-        <Navbar.Link
-          active
-          href="#"
-        >
-          <p>
-            Home
-          </p>
-        </Navbar.Link>
-        <Navbar.Link href="#">
-          About Me
-        </Navbar.Link>
-        <Navbar.Link href="#">
-          Projects
-        </Navbar.Link>
-        <Navbar.Link href="#">
-          Contact Me
-        </Navbar.Link>
-      </Navbar.Collapse>
-    </Navbar>
+    <Disclosure as="nav" className="bg-gray-800">
+      {({ open }) => (
+        <>
+          <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+            <div className="relative flex h-16 items-center justify-between">
+              <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+                {/* Mobile menu button*/}
+                <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                  <span className="sr-only">Open main menu</span>
+                  {open ? (
+                    <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
+                  ) : (
+                    <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
+                  )}
+                </Disclosure.Button>
+              </div>
+              <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+                <div className="hidden sm:ml-6 sm:block">
+                  <div className="flex space-x-4">
+                    {navigation.map((item) => (
+                      <a
+                        key={item.name}
+                        href={item.href}
+                        className={classNames(
+                          item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                          'rounded-md px-3 py-2 text-sm font-medium'
+                        )}
+                        aria-current={item.current ? 'page' : undefined}
+                      >
+                        {item.name}
+                      </a>
+                    ))}
+                  </div>
+                </div>
+              </div>
+              <div className="absolute inset-y-0 right-0 flex items-center gap-2 pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+                <DarkThemeToggle className='text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium' />
+                  <div>
+                    <img
+                        className="h-8 w-8 rounded-full"
+                        src="https://res.cloudinary.com/delgya5vj/image/upload/c_fill,g_face,w_300,h_300,r_max/v1687473528/doggos/My_project_5_akvlhm.png"
+                        alt=""
+                      />
+                  </div>
+              </div>
+            </div>
+          </div>
+
+          <Disclosure.Panel className="sm:hidden">
+            <div className="space-y-1 px-2 pb-3 pt-2">
+              {navigation.map((item) => (
+                <Disclosure.Button
+                  key={item.name}
+                  as="a"
+                  href={item.href}
+                  className={classNames(
+                    item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                    'block rounded-md px-3 py-2 text-base font-medium'
+                  )}
+                  aria-current={item.current ? 'page' : undefined}
+                >
+                  {item.name}
+                </Disclosure.Button>
+              ))}
+            </div>
+          </Disclosure.Panel>
+        </>
+      )}
+    </Disclosure>
   )
 }
+
 
 
